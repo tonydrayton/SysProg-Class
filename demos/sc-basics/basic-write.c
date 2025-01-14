77d5c011bf8d3ece26df0e02a00578d8653a8084
Brian Mitchell <dr.brian.mitchell@gmail.com>
2025-01-14 08:22:41 -0500

    asm demo

--- demos/sc-basics/basic-write.c
+++ demos/sc-basics/basic-write.c
@@ -1,5 +1,7 @@
 #include <unistd.h>
+#include <stdio.h>
 
 int main(){
-    write(STDOUT_FILENO, "Hello there\n", 12);
+    int rc = write(STDOUT_FILENO, "Hello there\n", 12);
+    printf("write rc = %d\n",rc);
 }
