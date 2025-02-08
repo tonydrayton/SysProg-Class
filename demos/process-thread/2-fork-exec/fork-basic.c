150657bcbaca5099042d3eee183bdcd76bcde1ad
Brian Mitchell <dr.brian.mitchell@gmail.com>
2025-02-08 09:50:29 -0500

    demo updates:

--- demos/process-thread/2-fork-exec/fork-basic.c
+++ demos/process-thread/2-fork-exec/fork-basic.c
@@ -41,19 +41,19 @@ int main()
         // lets print f_result
         printf("[p] The fork() command returned %d\n", f_result);
         printf("[p] My parent process id is %d\n", getpid());
-
         printf("[p] Waiting for child to finish...\n\n");
         wait(&c_result);
 
         // we can use a macro in the runtime library to extract
         // the status code from what wait_returns
+        printf("[p] The raw c_result value is %d\n", c_result);
         printf("[p] The child exit status is %d\n", WEXITSTATUS(c_result));
 
         // OR, even better we can understand what is going on.  c_result
         // is a 32 bit number encoded with several peices of information.
         // The status code is in the second byte.  We can extract it with
         // some bit manipulation and shifting....
-        c_result = (c_result >> 8) & 0xFF;
+        c_result = (c_result >> 8) & 0x000000FF;
         printf("[p] The child exited with code (manually extracted) %d\n", c_result);
 
         exit(22);
