e0d6397c20b449e2f3aaede3bf453c37a3be91be
bdlilley <114938118+bdlilley@users.noreply.github.com>
2025-02-10 14:27:37 -0500

    bats demo

---
+++ demos/_bl857/bats/example.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bats
+
+# CONCEPT: test names should be descriptive about what they do and what you expect
+@test "pwd should return 0" {
+    # CONCEPT: most of the time you need to use the BATS "run" command to execute something you want to make assertions about
+    run pwd
+
+    # CONCEPT: after using `run`, the $status variable contains the exit code of the command
+    # CONCEPT: asserts are performed using the bash test syntax (square braces with a boolean expression)
+    [ "$status" -eq 0 ]
+}
+
+@test "ls /dev/noexists returns an error" {
+    run ls /dev/noexists
+
+    # CONCEPT: echo commands are only printed on failure, so these diagnostic messages will only show if something goes wrong
+    # CONCEPT: experiement with this by changing the assertion below to be false, causing test failure
+    echo "captured output: $output\n"
+    echo "return status: $status\n"
+
+    [ "$status" -eq 1 ]
+}
+
+@test "ls /dev/null exists" {
+    run ls /dev/null
+
+    echo "captured output: $output\n"
+    echo "return status: $status\n"
+
+    # CONCEPT: single-line output can be tested with the `=` operator; note that BATS removes the trailing \n
+    [ "$output" = "/dev/null" ]
+}
+
+@test "multiline text example" {
+    run printf "1\n2\n3"
+
+    # CONCEPT: one way to test multi-line output is to transform it to a single string with no newlines
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+
+    echo "captured output: $output\n"
+    echo "stripped output: $stripped_output\n"
+    echo "return status: $status\n"
+    
+
+     # CONCEPT: run printed multiline output, but we were able to still test it by removing newlines and whitespace
+    [ "$stripped_output" = "123" ]
+}
+
+@test "heredoc test example" {
+    # CONCEPT: commands that read from stdin can be provided multi-line input using a "heredoc"
+    # CONCEPT: this is the same way in which you can test a "run" of `dsh`
+    run tee <<EOF
+hello,
+world
+EOF
+
+    stripped_output=$(echo "$output" | tr -d '[:space:]')
+
+    echo "captured output: $output\n"
+    echo "stripped output: $stripped_output\n"
+    echo "return status: $status\n"
+
+    [ "$stripped_output" = "hello,world" ]
+}
